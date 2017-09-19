9156407d60d185ac38e753041da80008d224854b
Shu Kobuchi <shukobuchi+github@gmail.com>
2017-09-19 10:59:00 +0900

    Update bitcoin-ruby-sample.rb

--- bitcoin-ruby-sample.rb
+++ bitcoin-ruby-sample.rb
@@ -16,10 +16,13 @@ def bitcoinRPC(method,param)
     JSON.parse(http.request(request).body)["result"]
 end
 
+puts "Enter Bitcoin Address"
+target_address = gets.chop
+puts target_address
+
 @walletinfo = bitcoinRPC('getwalletinfo',[])
 txcount = @walletinfo["txcount"]
-puts @walletinfo
-puts txcount
+
 @listtransactions = bitcoinRPC('listtransactions',["*", txcount])
 txids = []
 for i in 0..txcount-1 do
@@ -29,4 +32,35 @@ for i in 0..txcount-1 do
     end
 end
 
-puts @txids
+for j in 0..@txids.length-1 do
+    vin_flg = 0
+    vout_flg = 0
+    rawtransaction = bitcoinRPC('getrawtransaction',[@txids[j]])
+    @decodedtransaction = bitcoinRPC('decoderawtransaction',[rawtransaction])
+    vin_number = @decodedtransaction["vin"].length
+    for k in 0..vin_number-1 do
+        vin_txid = @decodedtransaction["vin"][k]["txid"]
+        vin_tx_vout = @decodedtransaction["vin"][k]["vout"]
+        vin_rawtransaction = bitcoinRPC('getrawtransaction',[vin_txid])
+        @vin_decodedtransaction = bitcoinRPC('decoderawtransaction',[vin_rawtransaction])
+        vin_address = @vin_decodedtransaction["vout"][vin_tx_vout]["scriptPubKey"]["addresses"][0]
+        if (vin_address == target_address)
+            vin_flg += 1
+        end  
+    end
+
+    vout_number =  @decodedtransaction["vout"].length
+    for l in 0..vout_number-1 do
+        if (@decodedtransaction["vout"][l]["scriptPubKey"]["type"] != "nulldata")
+            vout_address = @decodedtransaction["vout"][l]["scriptPubKey"]["addresses"][0]
+            if (vout_address == target_address)
+                vout_flg += 1
+            end
+        end
+    end
+    if (vin_flg >= 1 || vout_flg >= 1)
+        puts vin_flg
+        puts vout_flg
+        puts @txids[j]
+    end
+end
